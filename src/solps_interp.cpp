44f107f24fb84173ad3b4e227f89ec261a97cbc1
Timothy Younkin <tqd@Timothys-MacBook-Pro.local>
2021-01-26 11:25:08 -0500

    added thrust execution

--- src/solps_interp.cpp
+++ src/solps_interp.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 #include <cmath>
 #include <omp.h>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/execution_policy.h>
 
 void vectorPrint(std::string a, double D[])
 {
@@ -121,6 +124,60 @@ bool in_triangle(double r1, double z1, double r2, double z2, double r3, double z
           return in_triangle;
 }
 
+struct saxpy_functor
+{
+    int* index_triangle;
+    int nr;
+    int nz;
+    double* r;
+    double* z;
+    double* r1;
+    double* z1;
+    double* v1;
+    double* r2;
+    double* z2;
+    double* v2;
+    double* r3;
+    double* z3;
+    double* v3;
+    double* radius;
+    double* val;
+
+    saxpy_functor(int* _index_triangle, int _nr, int _nz,double* _r, double* _z,
+        double* _r1, double* _z1, double* _v1,
+        double* _r2, double* _z2, double* _v2,
+        double* _r3, double* _z3, double* _v3, double* _radius, double* _val) : 
+      index_triangle(_index_triangle), nr(_nr), nz(_nz), r(_r), z(_z),
+      r1(_r1), z1(_z1), v1(_v1),
+      r2(_r2), z2(_z2), v2(_v2),
+      r3(_r3), z3(_z3), v3(_v3), radius (_radius), val(_val) {}
+
+    __host__ __device__
+        void operator()(size_t index) const {
+          int jj = floor(index/nr);
+          int ii = index%nr;
+          int i = index_triangle[0];
+          //if(i > 0)
+          //{
+          //std::cout << "inds " << i << " " << ii << " " << jj << " " << index << std::endl;
+          //}
+          double dist = (r[ii] - r1[i])*(r[ii] - r1[i]) + (z[jj] - z1[i])*(z[jj] - z1[i]);
+           if(dist < radius[i]*radius[i])
+           {
+             bool in = in_triangle(r1[i],z1[i],r2[i],z2[i],r3[i],z3[i],r[ii],z[jj]);
+
+             if(in)
+             {
+               double a_point = interpolate_value(r1[i],z1[i],v1[i],
+                                                r2[i],z2[i],v2[i],
+                                                r3[i],z3[i],v3[i],
+                                                r[ii],z[jj]);
+               val[index] = a_point;
+             }
+           }
+        }
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
@@ -148,11 +205,15 @@ int main()
     ny = _ny.getSize();
 
     n_total = nx*ny*n8;
-
-    std::vector<double> r1(n_total), r2(n_total), r3(n_total),
-                        z1(n_total), z2(n_total), z3(n_total),
-                        v1(n_total), v2(n_total), v3(n_total),
-                        radius(n_total);
+    //std::vector<double> r1(n_total), r2(n_total), r3(n_total),
+    //                    z1(n_total), z2(n_total), z3(n_total),
+    //                    v1(n_total), v2(n_total), v3(n_total),
+    //                    radius(n_total);
+    
+    thrust::host_vector<double> r1_h(n_total), r2_h(n_total), r3_h(n_total),
+                        z1_h(n_total), z2_h(n_total), z3_h(n_total),
+                        v1_h(n_total), v2_h(n_total), v3_h(n_total),
+                        radius_h(n_total);
 
     netCDF::NcVar ncp_r1(ncp.getVar("r1"));
     netCDF::NcVar ncp_r2(ncp.getVar("r2"));
@@ -165,20 +226,44 @@ int main()
     netCDF::NcVar ncp_v3(ncp.getVar("v3"));
     netCDF::NcVar ncp_rad(ncp.getVar("radius"));
     
-    ncp_r1.getVar(&r1[0]);
-    ncp_r2.getVar(&r2[0]);
-    ncp_r3.getVar(&r3[0]);
-    ncp_z1.getVar(&z1[0]);
-    ncp_z2.getVar(&z2[0]);
-    ncp_z3.getVar(&z3[0]);
-    ncp_v1.getVar(&v1[0]);
-    ncp_v2.getVar(&v2[0]);
-    ncp_v3.getVar(&v3[0]);
-    ncp_rad.getVar(&radius[0]);
+    ncp_r1.getVar(&r1_h[0]);
+    ncp_r2.getVar(&r2_h[0]);
+    ncp_r3.getVar(&r3_h[0]);
+    ncp_z1.getVar(&z1_h[0]);
+    ncp_z2.getVar(&z2_h[0]);
+    ncp_z3.getVar(&z3_h[0]);
+    ncp_v1.getVar(&v1_h[0]);
+    ncp_v2.getVar(&v2_h[0]);
+    ncp_v3.getVar(&v3_h[0]);
+    ncp_rad.getVar(&radius_h[0]);
     ncp.close();
+    
+    thrust::device_vector<double> r1 = r1_h;
+    thrust::device_vector<double> r2 = r2_h;
+    thrust::device_vector<double> r3 = r3_h;
+    thrust::device_vector<double> z1 = z1_h;
+    thrust::device_vector<double> z2 = z2_h;
+    thrust::device_vector<double> z3 = z3_h;
+    thrust::device_vector<double> v1 = v1_h;
+    thrust::device_vector<double> v2 = v2_h;
+    thrust::device_vector<double> v3 = v3_h;
+    thrust::device_vector<double> radius = radius_h;
  
+    double* r1_pointer = thrust::raw_pointer_cast(&r1[0]);
+    double* z1_pointer = thrust::raw_pointer_cast(&z1[0]);
+    double* v1_pointer = thrust::raw_pointer_cast(&v1[0]);
+    double* r2_pointer = thrust::raw_pointer_cast(&r2[0]);
+    double* z2_pointer = thrust::raw_pointer_cast(&z2[0]);
+    double* v2_pointer = thrust::raw_pointer_cast(&v2[0]);
+    double* r3_pointer = thrust::raw_pointer_cast(&r3[0]);
+    double* z3_pointer = thrust::raw_pointer_cast(&z3[0]);
+    double* v3_pointer = thrust::raw_pointer_cast(&v3[0]);
+    double* radius_pointer = thrust::raw_pointer_cast(&radius[0]);
+    
     int nr = 440;
     int nz = 930;
+    thrust::counting_iterator<std::size_t> point_first(0);
+    thrust::counting_iterator<std::size_t> point_last(nr*nz);
     double r_start = 4.0;
     double r_end = 8.4;
     double z_start = -4.6;
@@ -187,17 +272,24 @@ int main()
     double dr = (r_end - r_start)/(nr - 1);
     double dz = (z_end - z_start)/(nz - 1);
 
-    std::vector<double> r(nr), z(nz), val(nr*nz);
+    thrust::host_vector<double> r_h(nr), z_h(nz), val_h(nr*nz);
 
     for(int i=0; i< nr; i++)
     {
-      r[i] = r_start + i*dr;
+      r_h[i] = r_start + i*dr;
     }
     
     for(int j=0; j < nz; j++)
     {
-      z[j] = z_start + j*dz;
+      z_h[j] = z_start + j*dz;
     }
+    
+    thrust::device_vector<double> r = r_h;
+    thrust::device_vector<double> z = z_h; 
+    thrust::device_vector<double> val(nr*nz,0.0);
+    double* r_pointer = thrust::raw_pointer_cast(&r[0]);
+    double* z_pointer = thrust::raw_pointer_cast(&z[0]);
+    double* val_pointer = thrust::raw_pointer_cast(&val[0]);
     
     //int nr = 1;
     //int nz = 1;
@@ -208,37 +300,41 @@ int main()
     double hollow_r1 = 7.3;
     double hollow_z0 = -0.7;
     double hollow_z1 = 2.35;
-#pragma omp parallel for collapse(3)
-    for(int i=0; i< n_total; i++)
-    {
-        for(int jj=0; jj < nz; jj++)
-        {
-          for(int ii=0; ii< nr; ii++)
-          {
-            //if((r[ii] > hollow_r0) && (r[ii] < hollow_r1) && (z[jj] > hollow_z0) && (z[jj] < hollow_z1) )
-            //{
-            //}
-            //else
-            //{
-              double dist = (r[ii] - r1[i])*(r[ii] - r1[i]) + (z[jj] - z1[i])*(z[jj] - z1[i]);
-              if(dist < radius[i]*radius[i])
-              {
-                bool in = in_triangle(r1[i],z1[i],r2[i],z2[i],r3[i],z3[i],r[ii],z[jj]);
+    thrust::device_vector<int> index_triangle(1,0);
+    int* triangle_pointer = thrust::raw_pointer_cast(&index_triangle[0]);
+    saxpy_functor spf(triangle_pointer,nr,nz,r_pointer,z_pointer,
+                      r1_pointer,z1_pointer,v1_pointer,
+                      r2_pointer,z2_pointer,v2_pointer,
+                      r3_pointer,z3_pointer,v3_pointer,radius_pointer,val_pointer);
+//#pragma omp parallel for collapse(3)
+  for(int i=0; i< n_total; i++)
+  {
+    //for(int jj=0; jj < nz; jj++)
+    //{
+    //  for(int ii=0; ii< nr; ii++)
+    //  {
+          index_triangle[0] = i;
+          thrust::for_each(thrust::device,point_first,point_last,spf);
+              //double dist = (r[ii] - r1[i])*(r[ii] - r1[i]) + (z[jj] - z1[i])*(z[jj] - z1[i]);
+              //if(dist < radius[i]*radius[i])
+              //{
+              //  bool in = in_triangle(r1[i],z1[i],r2[i],z2[i],r3[i],z3[i],r[ii],z[jj]);
 
-                if(in)
-                {
-                  double a_point = interpolate_value(r1[i],z1[i],v1[i],
-                                                   r2[i],z2[i],v2[i],
-                                                   r3[i],z3[i],v3[i],
-                                                   r[ii],z[jj]);
-                  val[jj*nr + ii] = a_point;
-                }
-              }
-            //}
-          }
-        }
+              //  if(in)
+              //  {
+              //    double a_point = interpolate_value(r1[i],z1[i],v1[i],
+              //                                     r2[i],z2[i],v2[i],
+              //                                     r3[i],z3[i],v3[i],
+              //                                     r[ii],z[jj]);
+              //    val[jj*nr + ii] = a_point;
+              //  }
+              //}
+    //      }
+    //    }
       
     }
+
+    val_h = val;
 
       netCDF::NcFile ncFile_out("interpolated_values.nc",
                          netCDF::NcFile::replace);
@@ -252,9 +348,9 @@ int main()
       netCDF::NcVar _gridz = ncFile_out.addVar("gridz", netCDF::ncDouble, _nz);
       netCDF::NcVar _vals = ncFile_out.addVar("values", netCDF::ncDouble, outdim);
 
-      _gridr.putVar(&r[0]);
-      _gridz.putVar(&z[0]);
-      _vals.putVar(&val[0]);
+      _gridr.putVar(&r_h[0]);
+      _gridz.putVar(&z_h[0]);
+      _vals.putVar(&val_h[0]);
       ncFile_out.close();
     return 0;
 }
