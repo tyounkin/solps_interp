988d30c93e065f47cc953194bec2c5a2458acf3d
atulkumar19 <atul.j1211@gmail.com>
2023-04-18 13:11:03 -0400

    This version works for Proto-MPEX SOLPS-ITER data

---
+++ build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.dir/app_intermediate_link.o"
+  "CMakeFiles/app.dir/src/app_generated_solps_interp.cpp.o"
+  "app"
+  "app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- build/make_solps_interp_cuda.sh
+++ build/make_solps_interp_cuda.sh
@@ -2,10 +2,12 @@
 source env.fusiont5.sh
 
 cmake -DTHRUST_INCLUDE_DIR=/home/tqd/Code/thrust/thrust \
-    -DNETCDF_CXX_INCLUDE_DIR=/home/tqd/Code/spack/opt/spack/linux-ubuntu18.04-x86_64/gcc-7.4.0/netcdf-cxx4-4.3.0-rokguzynggbzgzwzkx27v477ldmiz4ux/include \
-    -DNETCDF_CXX_LIBRARY=/home/tqd/Code/spack/opt/spack/linux-ubuntu18.04-x86_64/gcc-7.4.0/netcdf-cxx4-4.3.0-rokguzynggbzgzwzkx27v477ldmiz4ux/lib/libnetcdf_c++4.so \
-    -DBoost_DIR=/home/tqd/Code/boost/boostBuild \
-    -DBoost_INCLUDE_DIR=/home/tqd/Code/boost/boostBuild/include \
+    -DNETCDF_CXX_INCLUDE_DIR=$NETCDFCXX4INCLUDE \
+    -DNETCDF_CXX_LIBRARY=$NETCDFLIB_CPP \
+    -DNETCDF_DIR=$NETCDFDIR \
+    -DNETCDF_INCLUDE_DIR=$NETCDFINCLUDE \
+    -DNETCDF_LIBRARY=$NETCDFLIB \
+    -DNETCDF_CXX_INCLUDE_DIR=$NETCDFCXX4INCLUDE \
     -DLIBCONFIGPP_LIBRARY=$LIBCONFIGLIB \
     -DMPI_C_LIBRARIES=/cm/shared/apps/mpich/ge/gcc/64/3.2.1/lib \
     ..
